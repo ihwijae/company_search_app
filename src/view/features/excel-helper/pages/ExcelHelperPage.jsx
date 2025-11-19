e88815fb2beacbc317022c8805a7869f545a1ab6
leehwijae <lhj851@gmail.com>
2025-11-19 14:46:38 +0900

    이엔 이앤 보정

--- src/view/features/excel-helper/pages/ExcelHelperPage.jsx
+++ src/view/features/excel-helper/pages/ExcelHelperPage.jsx
@@ -390,6 +390,7 @@ const normalizeName = (value) => {
   name = name.replace(/^(주|\(주\)|㈜|주\)|\(합\))/, '');
   name = name.replace(/(주|\(주\)|㈜|주\)|\(합\))$/, '');
   name = name.replace(/이앤/g, '이엔');
+  name = name.replace(/앤/g, '엔');
   name = name.replace(/[^a-zA-Z0-9가-힣]/g, '');
   return name;
 };
