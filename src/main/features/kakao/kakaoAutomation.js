53237d0dc5cfcb159c52b11aec598c1a6b40749b
leehwijae <lhj851@gmail.com>
2026-01-23 17:52:56 +0900

    카카오톡연동

--- src/main/features/kakao/kakaoAutomation.js
+++ src/main/features/kakao/kakaoAutomation.js
@@ -76,16 +76,80 @@ $delayMs = if ($payload.delayMs) { [int]$payload.delayMs } else { 250 }
 $results = @()
 
 try { Add-Type -AssemblyName System.Windows.Forms } catch {}
-$wshell = New-Object -ComObject WScript.Shell
-$proc = $null
-try {
-  $proc = Get-Process -Name KakaoTalk -ErrorAction Stop | Select-Object -First 1
-} catch {
-  throw '카카오톡이 실행 중인지 확인해주세요.'
+
+Add-Type @\"
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+public static class Win32 {
+  [DllImport(\"user32.dll\", SetLastError=true)]
+  public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+  [DllImport(\"user32.dll\", SetLastError=true)]
+  public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+  [DllImport(\"user32.dll\", SetLastError=true)]
+  public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+  [DllImport(\"user32.dll\", SetLastError=true)]
+  public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+  [DllImport(\"user32.dll\", SetLastError=true)]
+  public static extern int GetWindowTextLength(IntPtr hWnd);
+  [DllImport(\"user32.dll\")]
+  public static extern bool SetForegroundWindow(IntPtr hWnd);
+  [DllImport(\"user32.dll\", CharSet=CharSet.Auto)]
+  public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, string lParam);
+  [DllImport(\"user32.dll\")]
+  public static extern bool PostMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 }
-if (-not $proc) { throw '카카오톡 프로세스를 찾을 수 없습니다.' }
-$null = $wshell.AppActivate($proc.Id)
-Start-Sleep -Milliseconds 350
+\"@
+
+$WM_SETTEXT = 0x000C
+$WM_KEYDOWN = 0x0100
+$VK_RETURN = 0x0D
+
+function Get-WindowText([IntPtr]$hWnd) {
+  $length = [Win32]::GetWindowTextLength($hWnd)
+  if ($length -le 0) { return \"\" }
+  $sb = New-Object System.Text.StringBuilder ($length + 1)
+  [void][Win32]::GetWindowText($hWnd, $sb, $sb.Capacity)
+  return $sb.ToString()
+}
+
+function Get-ClassName([IntPtr]$hWnd) {
+  $sb = New-Object System.Text.StringBuilder 128
+  [void][Win32]::GetClassName($hWnd, $sb, $sb.Capacity)
+  return $sb.ToString()
+}
+
+function Find-MainKakaoWindow {
+  $hwnd = [Win32]::FindWindowEx([IntPtr]::Zero, [IntPtr]::Zero, $null, $null)
+  while ($hwnd -ne [IntPtr]::Zero) {
+    $className = Get-ClassName $hwnd
+    if ($className -like \"*EVA_Window_Dblclk*\") {
+      $title = Get-WindowText $hwnd
+      if ($title -match '카카오톡|KakaoTalk') { return $hwnd }
+    }
+    $hwnd = [Win32]::FindWindowEx([IntPtr]::Zero, $hwnd, $null, $null)
+  }
+  return [IntPtr]::Zero
+}
+
+function Find-ChildWindowByText([IntPtr]$parent, [string]$text) {
+  $child = [Win32]::FindWindowEx($parent, [IntPtr]::Zero, $null, $null)
+  while ($child -ne [IntPtr]::Zero) {
+    $title = Get-WindowText $child
+    if ($title -and $title -like \"*${text}*\") { return $child }
+    $child = [Win32]::FindWindowEx($parent, $child, $null, $null)
+  }
+  return [IntPtr]::Zero
+}
+
+function Find-ChildWindowByClass([IntPtr]$parent, [string]$className) {
+  return [Win32]::FindWindowEx($parent, [IntPtr]::Zero, $className, $null)
+}
+
+$mainHwnd = Find-MainKakaoWindow
+if ($mainHwnd -eq [IntPtr]::Zero) { throw '카카오톡 창을 찾을 수 없습니다.' }
+ [void][Win32]::SetForegroundWindow($mainHwnd)
+ Start-Sleep -Milliseconds 200
 
 foreach ($item in $items) {
   $room = [string]$item.room
@@ -95,19 +159,38 @@ foreach ($item in $items) {
     continue
   }
   try {
-    $wshell.SendKeys('^f')
-    Start-Sleep -Milliseconds 200
-    $wshell.SendKeys('^a')
+    $onlineView = Find-ChildWindowByText $mainHwnd 'OnlineMainView'
+    if ($onlineView -eq [IntPtr]::Zero) { throw 'OnlineMainView를 찾을 수 없습니다.' }
+
+    $chatList = Find-ChildWindowByText $onlineView 'ChatRoomListView'
+    if ($chatList -eq [IntPtr]::Zero) {
+      $chatList = Find-ChildWindowByText $onlineView 'ContactListView'
+    }
+    if ($chatList -eq [IntPtr]::Zero) { throw '채팅 목록을 찾을 수 없습니다.' }
+
+    $edit = Find-ChildWindowByClass $chatList 'Edit'
+    if ($edit -eq [IntPtr]::Zero) { throw '채팅 검색창(Edit)을 찾을 수 없습니다.' }
+
+    [void][Win32]::SendMessage($edit, $WM_SETTEXT, [IntPtr]::Zero, $room)
+    Start-Sleep -Milliseconds 120
+    [void][Win32]::PostMessage($edit, $WM_KEYDOWN, [IntPtr]$VK_RETURN, [IntPtr]::Zero)
+    Start-Sleep -Milliseconds 300
+
+    $chatHwnd = [Win32]::FindWindow($null, $room)
+    if ($chatHwnd -eq [IntPtr]::Zero) { throw '채팅방 창을 찾지 못했습니다.' }
+
+    $richEdit = Find-ChildWindowByClass $chatHwnd 'RichEdit50W'
+    if ($richEdit -eq [IntPtr]::Zero) {
+      $richEdit = Find-ChildWindowByClass $chatHwnd 'Edit'
+    }
+    if ($richEdit -eq [IntPtr]::Zero) { throw '메시지 입력창을 찾지 못했습니다.' }
+
+    [void][Win32]::SetForegroundWindow($chatHwnd)
     Start-Sleep -Milliseconds 80
-    $wshell.SendKeys($room)
-    Start-Sleep -Milliseconds 200
-    $wshell.SendKeys('{ENTER}')
-    Start-Sleep -Milliseconds 280
-    [System.Windows.Forms.Clipboard]::SetText($msg)
-    $wshell.SendKeys('^v')
-    Start-Sleep -Milliseconds 200
-    $wshell.SendKeys('{ENTER}')
-    Start-Sleep -Milliseconds 200
+    [void][Win32]::SendMessage($richEdit, $WM_SETTEXT, [IntPtr]::Zero, $msg)
+    Start-Sleep -Milliseconds 80
+    [void][Win32]::PostMessage($chatHwnd, $WM_KEYDOWN, [IntPtr]$VK_RETURN, [IntPtr]::Zero)
+    Start-Sleep -Milliseconds 120
     $results += [pscustomobject]@{ room = $room; success = $true }
   } catch {
     $results += [pscustomobject]@{ room = $room; success = $false; error = $_.Exception.Message }
